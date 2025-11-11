7d2ff8c6a7ca34456ee072701be8bddcaa694a46
MutuyimanaEvelyne <evelynemutuyimana374@gmail.com>
2025-11-11 11:31:34 +0200

    Add files via upload

---
+++ seed_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees (first_name, last_name, department_id, salary, hire_date)
+VALUES ('Alice', 'Mugisha', 1, 5000, DATE '2021-03-01');
+INSERT INTO employees (first_name, last_name, department_id, salary, hire_date)
+VALUES ('John', 'Doe', 1, 6000, DATE '2020-07-15');
+INSERT INTO employees (first_name, last_name, department_id, salary, hire_date)
+VALUES ('Jane', 'Uwimana', 2, 7000, DATE '2022-01-20');
+INSERT INTO employees (first_name, last_name, department_id, salary, hire_date)
+VALUES ('Paul', 'Kamanzi', 3, 8000, DATE '2019-11-08');
+COMMIT;
