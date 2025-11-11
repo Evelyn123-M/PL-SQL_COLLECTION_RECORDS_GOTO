7ceb405c56507b52e0781ac3790ed4b622189d36
MutuyimanaEvelyne <evelynemutuyimana374@gmail.com>
2025-11-11 11:40:47 +0200

    Add files via upload

---
+++ examples_collections.sql
@@ -0,0 +1,14 @@
+Assoc array - Kigali pop: 1200000
+VARRAY elements:
+ nums(1) = 10
+ nums(2) = 20
+ nums(3) = 30
+VARRAY total = 60
+Nested table original elements:
+ nt(1) = 5
+ nt(3) = 15
+BULK_COLLECT fetched 2 salary rows for dept 1
+
+
+PL/SQL procedure successfully completed.
+
