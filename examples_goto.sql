e3c458e04699222d055bc0e72e0b7bbe889105e0
MutuyimanaEvelyne <evelynemutuyimana374@gmail.com>
2025-11-11 12:18:57 +0200

    Add files via upload

---
+++ examples_goto.sql
@@ -0,0 +1,16 @@
+In loop, counter = 1
+In loop, counter = 2
+Counter reached 3, jumping to skip_message label
+Arrived at skip_message label. counter = 3
+
+
+PL/SQL procedure successfully completed.
+
+t(1) = 1
+t(2) = 2
+Found 3 -- goto exit_all
+Exited early at i=3
+
+
+PL/SQL procedure successfully completed.
+
